6547b888a1996ee4147d468c97351bf42bf3d294
Hugh Brown <hughdbrown@yahoo.com>
2017-11-28 14:52:33 -0700

    pylint fixes and implement `has_module`

--- src/python/sed_python_func_debug.py
+++ src/python/sed_python_func_debug.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
-
-from sys import exit
+"""
+Streaming editor for modifying python files
+This script uses the `sed` python package to programmatically
+inject decorators at the head of function definitions.
+"""
+import sys
 import re
 from operator import itemgetter
 import logging
@@ -29,19 +33,44 @@ logging.basicConfig(level=logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
 
 
+# pylint: disable=too-few-public-methods
+# StreamEditor class has a minimal interface that a derived
+# class must implement, so pylint is cranky about the number of
+# methods implemented. Silence this warning.
+
 # Find python function definitions
 #   def func_name(args):
 # -----
 # Replace with function definition followed by logger call
 class StreamEditorInsertDebugAfterDef(StreamEditor):
+    """
+    Implement class for inserting debugging statements into a python file.
+    (Reimplemented to use decorators on methods.)
+    """
     table = [
         [[HAS_LOGGER, NEXT], ],
         [[DEF_FUNC, REPEAT], ],
     ]
 
-    def apply_match(self, i, dict_matches):
+    def apply_match(self, _, dict_matches):
+        """
+        Implement the `apply_match` method to the file.
+        """
+        def has_module(lines, match_line):
+            """
+            Check if a function definition that starts at line `match_line`
+            will have the __module__ attribute when decorated.
+
+            Some functions cannot be decorated because they are
+            missing relevant function attributes. Functions decorated
+            with @staticmethod and @classmethod are two of them.
+            """
+            prev_line = lines[match_line - 1]
+            return not prev_line.lstrip().startswith(
+                ("@staticmethod", "@classmethod")
+            )
+
         LOGGER.debug(dict_matches)
-        fmt = "{0[indent]}    {1}.info('{2}::{0[func_name]}')"
         matches = dict_matches["matches"]
         fns = [match for match in matches if match.get('func_name')]
         logger = [match for match in matches if match.get("name")]
@@ -51,22 +80,21 @@ class StreamEditorInsertDebugAfterDef(StreamEditor):
             # {'func_name': '__init__', 'line_no': 41, 'indent': '    '}
             LOGGER.debug(match)
             match_line = match["line_no"]
-            #s = fmt.format(match, logger_name, self.filename)
-            #self.append_range(match_line, [s])
-            s = "{0[indent]}@func_inspect".format(match)
-            self.insert_range(match_line, [s])
+            if has_module(self.lines, match_line):
+                insert_str = "{0[indent]}@func_inspect".format(match)
+                self.insert_range(match_line, [insert_str])
 
         # If you do this first, then all the lines will be off in
         # the function-name matches above.
         logger = logger[0]
-        logger_name, logger_line = logger["name"], logger["line_no"]
-        s = "from MMApp.decorators import func_inspect"
-        self.insert_range(logger_line, [s])
+        logger_line = logger["line_no"]
+        insert_str = "from MMApp.decorators import func_inspect"
+        self.insert_range(logger_line, [insert_str])
 
 def main():
+    """ Main entry point"""
     return call_main(StreamEditorInsertDebugAfterDef)
 
 
 if __name__ == '__main__':
-    exit(main())
-
+    sys.exit(main())
